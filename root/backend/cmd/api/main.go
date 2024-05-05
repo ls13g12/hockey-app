9c8cdef04c4e1cd85ecf40e4a3ca17e455121d9f
ls13g12 <82527268+ls13g12@users.noreply.github.com>
2024-05-05 14:50:37 +0100

    add request logger middleware (#3)
    * parse addr from command line flag

--- root/backend/cmd/api/main.go
+++ root/backend/cmd/api/main.go
@@ -1,42 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"net/http"
+	"flag"
+	"log/slog"
 	"os"
-
-	"github.com/ls13g12/hockey-app/root/backend/api/router"
+	"github.com/ls13g12/hockey-app/root/backend/api"
 )
 
-
-func NewServer() http.Handler {
-	mux := http.NewServeMux()
-	router.AddRoutes(mux)
-	var handler http.Handler = mux
-	return handler
+type application struct {
+	logger *slog.Logger
 }
+
+var cfg api.Config
 
 func main() {
-		srv := NewServer()
-	
-		httpServer := &http.Server{
-			Addr:    net.JoinHostPort("localhost", "8080"),
-			Handler: srv,
-		}
-	
-		fmt.Printf("listening on %s\n", httpServer.Addr)
-		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			fmt.Fprintf(os.Stderr, "error listening and serving: %s\n", err)
-		}
+	flag.StringVar(&cfg.Addr, "addr", ":8080", "HTTP network address")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	api.NewApiServer(
+		cfg,
+		logger,
+	)
 }
-
-
-
-
-
-
-
-
-
-
