e99836424f1874a1f6082013d031ecffe355d5bc
ls13g12 <82527268+ls13g12@users.noreply.github.com>
2024-05-05 16:43:47 +0100

    added Dockerfile (#4)
    Co-authored-by: lstoneman <lukejohnstoneman@gmail.com>

---
+++ root/backend/api/healthcheck.go
@@ -0,0 +1,21 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func healthcheckGet(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	payload := struct{
+		Message string `json:"message"`
+	}{
+		Message: "ok",
+	}
+	jsonData, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Printf("error is %v", err)
+	}
+	w.Write(jsonData)
+}
